d4eeca08393b7b5cebfcf6e11c2108802451812f
Leonardo Heredia <leonardoheredia0404@gmail.com>
2025-11-05 13:36:54 -0300

    Configuração inicial do projeto

---
+++ app/src/main/java/com/example/gestordeestoque/data/Categoria.kt
@@ -0,0 +1,7 @@
+package com.example.gestordeestoque.data
+
+data class Categoria(
+    val id: String = "",
+    val nome: String = "",
+    val descricao: String = ""
+)
---
+++ app/src/main/java/com/example/gestordeestoque/data/Produto.kt
@@ -0,0 +1,13 @@
+package com.example.gestordeestoque.data
+
+import com.google.firebase.Timestamp
+
+data class Produto(
+    val id: String = "",
+    val nome: String = "",
+    val descricao: String = "",
+    val quantidade: Int = 0,
+    val preco: Double = 0.0,
+    val dataCadastro: Timestamp = Timestamp.now(),
+    val categoriaId: String = ""
+)
